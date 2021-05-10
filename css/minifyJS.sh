11b7484dd8f23a882453ca58156e6e9917b8a89b
Nmalsha <malshis@yahoo.com>
2021-05-10 11:02:47 +0200

    working on nav

---
+++ css/minifyJS.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+
+yui-compressor bootstrap.css > bootstrap.min.css
+yui-compressor et-line.css > et-line.min.css
+yui-compressor font-awesome.css > font-awesome.min.css
---
+++ js/minifyJS.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+
+yui-compressor blocs.js > blocs.min.js
+yui-compressor bootstrap.js > bootstrap.min.js
+yui-compressor jquery-2.0.0.js > jquery-2.0.0.min.js
